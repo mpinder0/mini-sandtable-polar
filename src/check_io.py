a645570013ff5f0821f7e92cd03049b7e5b4c577
Matt Pinder <pindycore@hotmail.com>
2025-06-06 07:12:36 +0100

    try circuitpython method of reading gpio

--- src/check_io.py
+++ src/check_io.py
@@ -1,12 +1,19 @@
-import RPi.GPIO as GPIO
+import board
+import digitalio
+#import RPi.GPIO as GPIO
 import time
 
 REFERENCE_SENSOR_PIN = 4  # GPIO pin for the reference sensor
 
-GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
-GPIO.setup(REFERENCE_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+#GPIO.setup(REFERENCE_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+switch = digitalio.DigitalInOut(board.D4)
+switch.direction = digitalio.Direction.INPUT
+switch.pull = digitalio.Pull.UP  # Set pull-up resistor
 
 while True:
     # Check if the reference sensor is triggered
-    print(GPIO.input(REFERENCE_SENSOR_PIN))
+    #print(GPIO.input(REFERENCE_SENSOR_PIN))
+    print(switch.value)  # Print the state of the switch
     time.sleep(0.5)  # Sleep for a short duration to avoid flooding the output
